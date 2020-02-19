0f5932e9f98545b82c496f7809567e21e2b5f390
Tural Mustafayev <mustafayevtural46@gmail.com>
2020-02-19 21:26:12 +0400

    fsdgdfg

---
+++ BookStoreMVC/Models/ViewModels/Ad/FilterHomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreMVC.Models.ViewModels.Ad
+{
+    public class FilterHomeViewModel
+    {
+        public int page { get; set; } = 1;
+        public FilterOption filterOption { get; set; } = FilterOption.NewToOld;
+        public int GenreId { get; set; } = 0;
+    }
+}
