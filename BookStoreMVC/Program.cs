3eac615fd664b1c9023610ded7e389b9b55c315e
Tural Mustafayev <mustafayevtural46@gmail.com>
2020-02-20 11:08:35 +0400

    port changed

--- BookStoreMVC/Program.cs
+++ BookStoreMVC/Program.cs
@@ -20,6 +20,7 @@ namespace BookStoreMVC
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5100");
     }
 }
